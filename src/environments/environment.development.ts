1752982e0ff871302b7a2e605bcfc4904e8583ae
hariom-ojha23 <hariomojha2301@gmail.com>
2023-11-07 22:35:19 +0530

    fixing deployment issue using environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    githubUrl: 'https://api.github.com',
+    token: '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'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    githubUrl: 'https://api.github.com',
+    token: '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'
+};
